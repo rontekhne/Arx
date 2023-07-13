94ab5b5bd60d7a460580132f0f0eee2192cbd4eb
ron <rontekhne@users.noreply.github.com>
2023-07-13 16:10:27 -0300

    changed package name

---
+++ app/src/main/com/rontekhne/arx/Arx.java
@@ -0,0 +1,11 @@
+/* Arx */
+package com.rontekhne.arx;
+
+import org.libsdl.app.SDLActivity;
+
+public class Arx extends SDLActivity
+{
+    /**
+     * A sample wrapper class that just calls SDLActivity
+     */
+}
